38c6276092335011caaf1a04264a764e2e255d64
Альберт <sabir116rus@gmail.com>
2022-08-27 08:20:58 +0300

    replaced the variable

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,13 @@ public class Main {
     public static void main(String[] args) {
         int check = 100;
         int replenishment_amount = 1100;
-        int x;
         if (replenishment_amount > 1000) {
             int bonus = replenishment_amount / 100;
-            x = check + replenishment_amount + bonus;
-            System.out.println("Бонус равен " + bonus + " рублям, итоговая сумма на счету клиента - " + x + " рублей");
+            check = check + replenishment_amount + bonus;
+            System.out.println("Бонус равен " + bonus + " рублям, итоговая сумма на счету клиента - " + check + " рублей");
         } else {
-            x = replenishment_amount + check;
-            System.out.println("Бонусов нет, итоговая сумма на счету клиента " + x + " рублей");
+            check = replenishment_amount + check;
+            System.out.println("Бонусов нет, итоговая сумма на счету клиента " + check + " рублей");
         }
     }
 }
