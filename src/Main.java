1f6b503777afeecd5a29f3b097a0a205d0155b2e
Альберт <sabir116rus@gmail.com>
2022-09-01 10:10:26 +0300

    serviceable CamelCase

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         int check = 100;
-        int replenishment_amount = 1100;
-        if (replenishment_amount > 1000) {
-            int bonus = replenishment_amount / 100;
-            check = check + replenishment_amount + bonus;
+        int replenishmentAmount = 1100;
+        if (replenishmentAmount > 1000) {
+            int bonus = replenishmentAmount / 100;
+            check = check + replenishmentAmount + bonus;
             System.out.println("Бонус равен " + bonus + " рублям, итоговая сумма на счету клиента - " + check + " рублей");
         } else {
-            check = replenishment_amount + check;
+            check = replenishmentAmount + check;
             System.out.println("Бонусов нет, итоговая сумма на счету клиента " + check + " рублей");
         }
     }
